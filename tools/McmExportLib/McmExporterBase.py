647af34714f1174ba016035f3bc4f277c5d12eec
iancohn <60224067+iancohn@users.noreply.github.com>
2026-02-19 15:20:08 -0500

    chore: :poop: Remove force of new gssapi auth per query

--- tools/McmExportLib/McmExporterBase.py
+++ tools/McmExportLib/McmExporterBase.py
@@ -219,7 +219,6 @@ class McmExporterBase(dict):
         """Construct a GSSAPI auth object from the retrieved
         details
         """
-        self.auth = None
         if self.__getattribute__('auth') is not None and \
             isinstance(self.auth, HTTPSPNEGOAuth):
             return self.auth
